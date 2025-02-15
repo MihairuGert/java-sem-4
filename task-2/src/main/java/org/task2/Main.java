8249adeaed0a2cd905b74045ebdbac7d8732ab5a
MihairuGert <vertygert4949@gmail.com>
2025-02-15 11:33:40 +0700

    test

--- task-2/src/main/java/org/task2/Main.java
+++ task-2/src/main/java/org/task2/Main.java
@@ -2,6 +2,6 @@ package org.task2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("git test");
     }
 }
