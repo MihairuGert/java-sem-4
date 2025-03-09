d2ab20f306945b33d5248fb393836b638e2e3f14
MihairuGert <vertygert4949@gmail.com>
2025-03-09 17:48:26 +0700

    v0.10.3 fixed local sound

--- task-3/src/main/java/task3/sound/SoundPlayer.java
+++ task-3/src/main/java/task3/sound/SoundPlayer.java
@@ -8,9 +8,13 @@ import java.net.URL;
 public class SoundPlayer {
     public static void playShoot() {
         try {
-            Clip clip = AudioSystem.getClip();
             URL shootURL = SoundPlayer.class.getResource("/shoot1.wav");
-            clip.open(AudioSystem.getAudioInputStream(new File(shootURL.getPath())));
+            if (shootURL == null) {
+                return;
+            }
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(shootURL));
             clip.start();
         } catch (Exception e) {
             System.out.println("Cannot find the file.");
